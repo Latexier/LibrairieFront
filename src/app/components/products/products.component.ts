9376c51bf93eabea408606c2d4285b4a7e707e82
AnThoNy Lopez <anthonychien@hotmail.fr>
2025-10-06 10:01:20 +0200

    Services + models

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LivreService } from '../../services/livre.service';
+import { Livre } from '../../models/livre.model';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  livres: Livre[] = [];
+  loading = true;
 
+  constructor(private livreService: LivreService) {}
+
+  ngOnInit(): void {
+    this.livreService.getAll().subscribe({
+      next: (data) => {
+        this.livres = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Erreur :', err);
+        this.loading = false;
+      }
+    });
+  }
 }
---
+++ src/app/models/client.model.ts
@@ -0,0 +1,7 @@
+export interface Client {
+  id: number;
+  nom: string;
+  prenom: string;
+  password: string;
+  mail: string;
+}
