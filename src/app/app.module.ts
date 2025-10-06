3f4b02ae4b2326f064a57908668155156e3e330e
latexier <la.texier@gmail.com>
2025-10-06 10:01:27 +0200

    Création de la page contact + remplissage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ProductsComponent } from './components/products/products.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProductsComponent
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
