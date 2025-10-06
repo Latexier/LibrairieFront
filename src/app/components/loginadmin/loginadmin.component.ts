f33802667ee7f857e77b8b521437122380310ea8
latexier <la.texier@gmail.com>
2025-10-06 14:46:21 +0200

    Ajout du back des pages de login + changement dans la navbar de "se connecter" en "mon compte" même si ça rammène à la page de connexion pour l'instant

--- src/app/components/loginadmin/loginadmin.component.ts
+++ src/app/components/loginadmin/loginadmin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Admin } from 'src/app/models/admin.model';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-loginadmin',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./loginadmin.component.css']
 })
 export class LoginadminComponent {
+  login:string;
+  password:string;
+  a:Admin;
+  message:string;
 
+  constructor(private srv:AdminService){}
+  ngOnInit():void{}
+
+  connexion(){
+    this.srv.login(this.login,this.password).subscribe({
+      next:data=>{
+        this.a=data;
+        this.message="Login réussi";
+        sessionStorage.setItem("user",JSON.stringify(this.a));
+      },
+      error:(err)=>{
+        this.message="Mail ou mot de passe incorrect";
+      }
+    });
+  }
 }
