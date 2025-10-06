4f9df710f57e939d4d2e3dd29cd9eebec37f22a3
latexier <la.texier@gmail.com>
2025-10-06 09:17:57 +0200

    Ajout de page + visuels correspondants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
+import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
   {path:'', component:IndexComponent},
-  
+  {path:'products',component:ProductsComponent}
 ];
 
 @NgModule({
